589b1981712e0e8c83e230def9cb1d33e8cf577b
williamscodigo <willliamscodigo@gmail.com>
2025-01-09 16:27:22 -0500

    resolved error of undefined in seeds/cleadDb.ts file

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,9 +3,12 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    let modelExists = [];
+    if (models[modelName] && models[modelName].db && models[modelName].db.db) {
+        modelExists = await models[modelName].db.db.listCollections({
+          name: collectionName
+        }).toArray();
+    }
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
